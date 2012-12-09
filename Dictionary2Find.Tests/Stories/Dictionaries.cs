ee5aeb0b81dd7ae84f0dfd1ea09b8ff78c13d26f
Henrik Lindström <lindstrom.henrik@gmail.com>
2012-12-09 18:36:17 +0100

    Added story for filtering on geolocation.

--- Dictionary2Find.Tests/Stories/Dictionaries.cs
+++ Dictionary2Find.Tests/Stories/Dictionaries.cs
@@ -91,4 +91,83 @@ namespace Dictionary2Find.Tests.Stories
             public Dictionary<string, string> MetadataDictionary { get; set; }
         }
     }
+
+    public class DictionariesWithGeolocation
+    {
+        [Fact]
+        public void FilterByValueInDictionary()
+        {
+            new Story("Filter by matching a key/geolocation in a dictionary")
+                .InOrderTo("be able to filter on a geolocation of a specific key in a dictionary")
+                .AsA("developer")
+                .IWant("to be able to map dictionaries to their correct value type")
+                .WithScenario("map dictionaries to their correct value type")
+                .Given(IHaveAClient)
+                    .And(IHaveMappedTypesToDictionaryKeys)
+                    .And(IHaveAStore)
+                    .And(TheStoreHasALocationInStockholm)
+                    .And(IHaveIndexedTheStoreObject)
+                    .And(IHaveWaitedForASecond)
+                .When(ISearchForAStoreWithin1kmFromSergelsTorg)
+                .Then(IShouldGetASingleHit)
+                .Execute();
+        }
+
+        protected IClient client;
+        void IHaveAClient()
+        {
+            client = Client.CreateFromConfig();
+        }
+
+        void IHaveMappedTypesToDictionaryKeys()
+        {
+            client.Conventions.ContractResolver.ContractInterceptors.Add(new IncludeTypeNameInDictionaryKeyFieldNameConvention());
+        }
+
+        private Store store;
+        void IHaveAStore()
+        {
+            store = new Store();
+        }
+
+        void TheStoreHasALocationInStockholm()
+        {
+            store.Locations.Add("Stockholm", new GeoLocation(59.33265, 18.06468));
+        }
+
+        void IHaveIndexedTheStoreObject()
+        {
+            client.Index(store);
+        }
+
+        void IHaveWaitedForASecond()
+        {
+            Thread.Sleep(1000);
+        }
+
+        SearchResults<Store> result;
+        void ISearchForAStoreWithin1kmFromSergelsTorg()
+        {
+            result = client.Search<Store>()
+                        .Filter(x => x.Locations["Stockholm"].WithinDistanceFrom(new GeoLocation(59.33234, 18.06291), 1.Kilometers()))
+                        .GetResult();
+        }
+
+        void IShouldGetASingleHit()
+        {
+            result.TotalMatching.Should().Be(1);
+        }
+
+        public class Store
+        {
+            public Store()
+            {
+                Locations = new Dictionary<string, GeoLocation>();
+            }
+
+            public string Name { get; set; }
+
+            public Dictionary<string, GeoLocation> Locations { get; set; }
+        }
+    }
 }
