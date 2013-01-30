e0bfdc41e757e8ca5bf31128f4a2bf773be72a2c
Henrik Lindström <lindstrom.henrik@gmail.com>
2013-01-30 14:27:13 +0100

    Added conventions/interceptors.

---
+++ Dictionary2Find/ClientConventions/DictionaryClientConventionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EPiServer.Find;
+
+namespace Dictionary2Find.ClientConventions
+{
+    public static class DictionaryClientConventionsExtensions
+    {
+        public static void AddDictionaryConventions(this IClientConventions conventions)
+        {
+            conventions.ContractResolver.ContractInterceptors.Add(new DictionaryInterceptor());
+        }
+    }
+}
